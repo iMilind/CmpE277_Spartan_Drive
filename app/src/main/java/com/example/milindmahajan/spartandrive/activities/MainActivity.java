ffae8ba005c62bdff769bf98f950ac6bbecd1461
Milind Mahajan <milind.mahajan@sjsu.edu>
2015-11-17 01:58:53 -0800

    hierarchy setup
    Signed-off-by: Milind Mahajan <milind.mahajan@sjsu.edu>

--- app/src/main/java/com/example/milindmahajan/spartandrive/activities/MainActivity.java
+++ app/src/main/java/com/example/milindmahajan/spartandrive/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.milindmahajan.spartandrive;
+package com.example.milindmahajan.spartandrive.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.milindmahajan.spartandrive.R;
 
 public class MainActivity extends AppCompatActivity {
 
