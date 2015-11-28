42020f12d5cfb5ab70c0fac3da031be993b41307
jatinhchanchlani <jatin.chanchlani@sjsu.edu>
2015-11-28 12:21:55 -0800

    Dropbox Integration

--- app/src/main/java/com/example/milindmahajan/spartandrive/activities/MainActivity.java
+++ app/src/main/java/com/example/milindmahajan/spartandrive/activities/MainActivity.java
@@ -1,18 +1,144 @@
 package com.example.milindmahajan.spartandrive.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
 import com.example.milindmahajan.spartandrive.R;
+import com.example.milindmahajan.spartandrive.utils.Common;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+    private boolean mLoggedIn, onResume;
+
+    private DropboxAPI<AndroidAuthSession>  mApi;
+
+    private Button dropboxLogin = null;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AndroidAuthSession session = buildSession();
+        mApi = new DropboxAPI<AndroidAuthSession>(session);
+        mApi.getSession().startOAuth2Authentication(MainActivity.this);
+       /* dropboxLogin = (Button)findViewById(R.id.dropbox_login);
+        dropboxLogin.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+
+        );*/
+    }
+
+    private AndroidAuthSession buildSession() {
+        AppKeyPair appKeyPair = new AppKeyPair(Common.APP_KEY, Common.APP_SECRET);
+        AndroidAuthSession session = null;
+        String[] stored = getKeys();
+        if (stored != null) {
+            AccessTokenPair accessToken = new AccessTokenPair(stored[0],
+                    stored[1]);
+            session = new AndroidAuthSession(appKeyPair, Common.ACCESS_TYPE,
+                    accessToken);
+        } else {
+            session = new AndroidAuthSession(appKeyPair, Common.ACCESS_TYPE);
+        }
+        return session;
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        if (mApi.getSession().authenticationSuccessful()) {
+            try {
+                // Required to complete auth, sets the access token on the session
+                mApi.getSession().finishAuthentication();
+
+                String accessToken = mApi.getSession().getOAuth2AccessToken();
+                System.out.print(accessToken);
+                storeKeys("oauth2:", accessToken);
+                setLoggedIn(onResume);
+            } catch (IllegalStateException e) {
+                Log.i("DbAuthLog", "Error authenticating", e);
+                showToast("Couldn't authenticate with Dropbox:"
+                        + e.getLocalizedMessage());showToast("Couldn't authenticate with Dropbox:"
+                        + e.getLocalizedMessage());
+            }
+        }
+    }
+
+    private void showToast(String msg) {
+        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        error.show();
+    }
+    public void setLoggedIn(boolean loggedIn) {
+        mLoggedIn = loggedIn;
+        if (loggedIn) {
+            /*UploadFile upload = new UploadFile(Main.this, mApi, DIR, f);
+            upload.execute();
+            */
+            onResume = false;
+
+        }
+    }
+    private void storeKeys(String key, String secret) {
+        SharedPreferences prefs = getSharedPreferences(
+                Common.ACCOUNT_PREFS_NAME, 0);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString(Common.ACCESS_KEY_NAME, key);
+        edit.putString(Common.ACCESS_SECRET_NAME, secret);
+        edit.commit();
+    }
+    private void logOut() {
+        mApi.getSession().unlink();
+
+        clearKeys();
+    }
+
+    private void clearKeys() {
+        SharedPreferences prefs = getSharedPreferences(
+                Common.ACCOUNT_PREFS_NAME, 0);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.clear();
+        edit.commit();
+    }
+
+    private String[] getKeys() {
+        SharedPreferences prefs = getSharedPreferences(
+                Common.ACCOUNT_PREFS_NAME, 0);
+        String key = prefs.getString(Common.ACCESS_KEY_NAME, null);
+        String secret = prefs.getString(Common.ACCESS_SECRET_NAME, null);
+        if (key != null && secret != null) {
+            String[] ret = new String[2];
+            ret[0] = key;
+            ret[1] = secret;
+            return ret;
+        } else {
+            return null;
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/milindmahajan/spartandrive/utils/Common.java
@@ -0,0 +1,47 @@
+package com.example.milindmahajan.spartandrive.utils;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.dropbox.client2.session.Session;
+
+/**
+ * Created by Jatin on 11/28/2015.
+ */
+public class Common
+{
+    final static public String APP_KEY = "3cne6bs9s632jnk";
+    final static public String APP_SECRET = "58s3nww0d70sc0f";
+
+    final static public Session.AccessType ACCESS_TYPE = Session.AccessType.DROPBOX;
+    final static public String ACCOUNT_PREFS_NAME = "prefs";
+    final static public String ACCESS_KEY_NAME = "ACCESS_KEY";
+    final static public String ACCESS_SECRET_NAME = "ACCESS_SECRET";
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void showNetworkAlert(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Network Alert");
+        builder.setMessage("Please check your network connection and try again");
+        builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+}
