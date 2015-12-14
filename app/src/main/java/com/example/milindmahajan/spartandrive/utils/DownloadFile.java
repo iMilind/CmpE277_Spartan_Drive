358d21c6c3f49ec7f3c92f3413a4f8efb777da55
milind maha <milind.mahajan@sjsu.edu>
2015-12-13 19:59:05 -0800

    code refactored. file preview activity deleted. added intents to launch file depending on mime type
    Signed-off-by: milind maha <milind.mahajan@sjsu.edu>

--- app/src/main/java/com/example/milindmahajan/spartandrive/utils/DownloadFile.java
+++ app/src/main/java/com/example/milindmahajan/spartandrive/utils/DownloadFile.java
@@ -2,20 +2,11 @@ package com.example.milindmahajan.spartandrive.utils;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.widget.Toast;
 
 import com.dropbox.client2.DropboxAPI;
-import com.dropbox.client2.ProgressListener;
 import com.dropbox.client2.exception.DropboxException;
-import com.dropbox.client2.exception.DropboxFileSizeException;
-import com.dropbox.client2.exception.DropboxIOException;
-import com.dropbox.client2.exception.DropboxParseException;
-import com.dropbox.client2.exception.DropboxPartialFileException;
-import com.dropbox.client2.exception.DropboxServerException;
-import com.dropbox.client2.exception.DropboxUnlinkedException;
 import com.example.milindmahajan.spartandrive.model.DropboxItem;
 
 import java.io.BufferedInputStream;
@@ -24,8 +15,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 
 /**
  * Created by Jatin on 12/13/2015.
@@ -41,19 +30,27 @@ public class DownloadFile extends AsyncTask<Void, Long, Boolean>{
     private long mFileLen;
     AsyncResponse response;
 
-
     public interface AsyncResponse {
 
         void processFinish(boolean result);
     }
 
-    public DownloadFile(Context c, DropboxItem d, AsyncResponse response)
-    {
+    public DownloadFile(Context c, DropboxItem d, AsyncResponse response) {
+
         mContext = c;
         item = d;
         mFileLen = 1000;
         mApi = Common.getDropboxObj();
         this.response = response;
+
+        invokeProgressDialog();
+    }
+
+    public void invokeProgressDialog() {
+
+        mDialog = new ProgressDialog(mContext);
+        mDialog.setMessage("Downloading ");
+        mDialog.show();
     }
 
     @Override
@@ -62,6 +59,7 @@ public class DownloadFile extends AsyncTask<Void, Long, Boolean>{
         File rootDirect = new File(Common.getLocalRoot());
 
         if(!rootDirect.exists()) {
+
             boolean result = rootDirect.mkdir();
             if(rootDirect.mkdir()); //directory is created;
         }
@@ -71,30 +69,35 @@ public class DownloadFile extends AsyncTask<Void, Long, Boolean>{
 
             copy(item, localFile);
         }
+
         return Boolean.TRUE;
     }
 
     @Override
     protected void onPostExecute(Boolean result) {
 
+        mDialog.dismiss();
         if (result) {
+
             this.response.processFinish(true);
         } else {
+
             showToast(mErrorMsg);
         }
     }
 
     private void showToast(String msg) {
+
         Toast error = Toast.makeText(mContext, msg, Toast.LENGTH_LONG);
         error.show();
     }
+
     private void copy(final DropboxItem fileSelected, final File localFile) {
 
         BufferedInputStream br = null;
         BufferedOutputStream bw = null;
         DropboxAPI.DropboxInputStream fd;
         try {
-
 
             localFile.createNewFile();
             fd = mApi.getFileStream(fileSelected.getPath(),
@@ -106,52 +109,40 @@ public class DownloadFile extends AsyncTask<Void, Long, Boolean>{
             byte[] buffer = new byte[4096];
             int read;
             while (true) {
+
                 read = br.read(buffer);
                 if (read <= 0) {
+
                     break;
                 }
+
                 bw.write(buffer, 0, read);
             }
-
         } catch (DropboxException e) {
+
             e.printStackTrace();
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
+
             e.printStackTrace();
         } catch (IOException e) {
+
             e.printStackTrace();
         } finally {
+
             if (bw != null) {
+
                 try {
+
                     bw.close();
                     if (br != null) {
+
                         br.close();
                     }
                 } catch (IOException e) {
+
                     e.printStackTrace();
                 }
             }
-
         }
-
-
     }
-
-    /*private void showFileExitsDialog(final Entry fileSelected,
-                                     final File localFile) {
-        AlertDialog.Builder alertBuilder = new Builder(DropboxDownload.this);
-        alertBuilder.setMessage(Constants.OVERRIDEMSG);
-        alertBuilder.setPositiveButton("Ok",
-                new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        copy(fileSelected, localFile);
-                    }
-                });
-        alertBuilder.setNegativeButton("Cancel", null);
-        alertBuilder.create().show();
-
-    }
-*/
 }
