5c338b340a7828f9920a6d4279ab61b53fad5b75
Deepak Kole <deepakrkole@gmail.com>
2015-12-13 21:47:02 -0800

    Update API keys

--- app/src/main/java/com/example/milindmahajan/spartandrive/utils/Common.java
+++ app/src/main/java/com/example/milindmahajan/spartandrive/utils/Common.java
@@ -21,8 +21,8 @@ import java.util.ArrayList;
  */
 public class Common
 {
-    final static public String APP_KEY = "ng6nw88gthunoyx";
-    final static public String APP_SECRET = "qlh6xb7urnhcw6m";
+    final static public String APP_KEY = "xxxx";
+    final static public String APP_SECRET = "xxxx";
 
     final static public String ACCOUNT_PREFS_NAME = "prefs";
     final static public String ACCESS_KEY_NAME = "ACCESS_KEY";
